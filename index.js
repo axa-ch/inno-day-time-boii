87c737e868677398bba4e90a946ac8969934184b
Luca Mele <lucamele1@hotmail.com>
2020-01-20 15:29:01 +0100

    Date Stepper done, no css

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ import "./src/js/date-stepper.js";
 const { LitElement, html } = window;
 
 class TimeTracker extends LitElement {
+  static get properties() {
+    return {
+      date: { type: String, reflect: true }
+    };
+  }
+
+  handleChange = ({ detail: { date } }) => {
+    this.date = date.toString();
+  };
+
   render() {
     return html`
       <header>
-        <date-stepper mood="great"></date-stepper>
+        <date-stepper @change=${this.handleChange}></date-stepper>
       </header>
       <article>
-        TODO
+        <time-manager date=${this.date}></time-manager>
       </article>
     `;
   }
--- src/js/date-stepper.js
+++ src/js/date-stepper.js
@@ -1,27 +1,101 @@
 const { LitElement, html, css } = window;
 
+const PREV = "prev";
+const NEXT = "next";
+const DEFAULT_LOCALE = "de-CH";
+
+const sameDay = (d1, d2) =>
+  d1.getFullYear() === d2.getFullYear() &&
+  d1.getMonth() === d2.getMonth() &&
+  d1.getDate() === d2.getDate();
+
 class DateStepper extends LitElement {
   static get properties() {
     return {
-      mood: { type: String }
+      value: { type: String, reflect: true }
     };
   }
 
   static get styles() {
-    return css`
-      .mood {
-        color: green;
+    return css``;
+  }
+
+  constructor() {
+    super();
+    if (this.value) {
+      this.date = new Date(this.value);
+    } else {
+      this.date = new Date();
+    }
+  }
+
+  fireEvent() {
+    // fire up change
+    const event = new CustomEvent("change", {
+      detail: {
+        date: this.date
       }
-    `;
+    });
+    this.dispatchEvent(event);
+  }
+
+  handleClick = (mode = NEXT) => {
+    this.date.setDate(this.date.getDate() + (mode === PREV ? -1 : 1));
+    this.value = this.date.toString();
+    this.fireEvent();
+  };
+
+  getDate = () => {
+    return this.date.toLocaleDateString(DEFAULT_LOCALE, {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    });
+  };
+
+  getHelpText = () => {
+    const today = new Date();
+
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    if (sameDay(this.date, today)) {
+      return "Heute";
+    }
+    if (sameDay(this.date, yesterday)) {
+      return "Gestern";
+    }
+    if (sameDay(this.date, tomorrow)) {
+      return "Morgen";
+    }
+
+    return this.date.toLocaleDateString(DEFAULT_LOCALE, {
+      month: "numeric",
+      day: "numeric"
+    });
+  };
+
+  // fire the inital date so that the other components know
+  firstUpdated() {
+    this.fireEvent();
   }
 
   render() {
     return html`
       <div>
-        <button>〈</button>
-        <button>〉</button>
+        <button @click="${() => this.handleClick(PREV)}">〈</button>
+        <div>
+          <p>${this.getDate()}</p>
+          <p>
+            ${this.getHelpText()}
+          </p>
+        </div>
+        <button @click="${() => this.handleClick(NEXT)}">〉</button>
       </div>
-      Web Components are <span class="mood">${this.mood}</span>!
     `;
   }
 }
