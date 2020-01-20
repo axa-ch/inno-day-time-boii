9967fb7dda562a31b92ae23cd7f4cb6476454bcd
Luca Mele <lucamele1@hotmail.com>
2020-01-20 14:11:33 +0100

    Init structure

---
+++ index.js
@@ -0,0 +1,25 @@
+const { LitElement } = window;
+
+class MyElement extends LitElement {
+  static get properties() {
+    return {
+      mood: { type: String }
+    };
+  }
+
+  static get styles() {
+    return css`
+      .mood {
+        color: green;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+      Web Components are <span class="mood">${this.mood}</span>!
+    `;
+  }
+}
+
+customElements.define("my-element", MyElement);
