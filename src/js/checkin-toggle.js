8316ff6cc6df839711e285eae9565bb79f9417a2
Noah Liechti <noah.liechti@gmail.com>
2020-01-20 15:47:29 +0100

    styling toggle-button

--- src/js/checkin-toggle.js
+++ src/js/checkin-toggle.js
@@ -14,13 +14,35 @@ class CheckinToggle extends LitElement {
   static get styles() {
     return css`
       .container {
-        border: 1px solid grey;
+        padding: 5px;
+        display: flex;
+        flex-wrap: nowrap;
+        position: fixed;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        color: #fff;
+        box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.5);
+        align-items: center;
+      }
+      .not-active {
+        height: 70px;
+        background-color: #f2f2f2;
+        color: #000;
+        border: 2px solid #9e9e9e;
       }
       .active {
+        height: 80px;
         box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.5);
+        background-color: #00008f;
+        border-color: #00008f;
+        color: #fff;
+        cursor: pointer;
       }
       .toggle-button {
-        height: 40px;
+        flex: 1;
+        font-size: 28px;
+        margin: 5px;
       }
     `;
   }
@@ -29,7 +51,7 @@ class CheckinToggle extends LitElement {
     return html`
       <div class="container">
         <button class="toggle-button active">Ein</button>
-        <button class="toggle-button">Aus</button>
+        <button class="toggle-button not-active">Aus</button>
       </div>
     `;
   }
