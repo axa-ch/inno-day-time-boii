f912df036f960482bba13e78dcbc7c402f564624
Luca Mele <lucamele1@hotmail.com>
2020-01-20 16:27:20 +0100

    Add persistence logic

--- src/js/persistence.js
+++ src/js/persistence.js
@@ -1,10 +1,11 @@
 // helpers
+const DEFAULT_DAILY = 8.4;
+const ACCUMULATED_HOURS_KEY = "accumulatedHours";
 const formatDateAsKey = date =>
   `${date.getFullYear()}-0${date.getMonth()}-0${date.getDate()}`;
 const timeToDecimal = (hours, minutes) =>
   parseInt(hours, 10) + parseInt(minutes, 10) / 60; // hours in 24h format
-
-let inst = null;
+let inst = null; // singleton instance
 // class
 export default class Persistence {
   static FROM = 0;
@@ -38,11 +39,38 @@ export default class Persistence {
   previous() {
     return this.next(-1);
   }
-  daily(hours) {}
-  yearly(hours) {}
+  daily(hours) {
+    this.getItems(); // sets dateKey, entry as side-effect
+    const { entry, dateKey } = this;
+    if (hours === undefined) {
+      return entry.daily || DEFAULT_DAILY;
+    }
+    entry.daily = hours;
+    this._daily = hours;
+    const value = JSON.stringify(entry);
+    localStorage.setItem(dateKey, value);
+    return hours;
+  }
+  yearly(deltaHours) {
+    const _accumulatedHours =
+      localStorage.getItem(ACCUMULATED_HOURS_KEY) || "0.0";
+    const accumulatedHours = parseFloat(_accumulatedHours);
+    if (deltaHours === undefined) {
+      return accumulatedHours;
+    }
+    const newAccumulatedHours = accumulatedHours + deltaHours;
+    localStorage.setItem(ACCUMULATED_HOURS_KEY, newAccumulatedHours);
+    return newAccumulatedHours;
+  }
   add(time, which) {
     // time format: hh:mm
     const items = this.getItems(); // sets dateKey, entry as side-effect
+    if (time === undefined) {
+      const now = new Date();
+      const _minutes = now.getMinutes();
+      const _hours = now.getHours();
+      time = `${_hours}:${_minutes}`;
+    }
     const [ok, hours, minutes] = `${time}`.match(/^(\d\d?):(\d\d)$/);
     if (!ok) {
       return false;
