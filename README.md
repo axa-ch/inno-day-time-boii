cdfaee00b93d3f59c3000b6bd07dfbff5ddba794
Markus Walther <mw@Markuss-MacBook-Pro-2.local>
2020-08-18 14:39:47 +0200

    Fix bug in nextDay function

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # inno-day-time-boii
+
+## Technical details
+
+### Data structures
+
+This app manipulates dates and times.
+
+**Dates** are serialized in `YYYY-MM-DD` format. This has the advantage of
+being human-readable and debug-friendly.
+
+Under a given date, a list of **time pairs** is stored. This is an
+array `[timePair0, ..., timePairN]`.
+
+Each **time pair** is another length-2 array
+`[checkInTime, checkOutTime]`. Both the check-in and the check-out
+time are decimal numbers corresponding of 24-hour time events,
+e.g. `8.5` for 8:30 AM. Representing time events as numbers facilitates
+simple calculation of work hours via subtraction over a time pair and accumulated
+daily or yearly work hours via addition.
+
--- src/js/date-manipulation.js
+++ src/js/date-manipulation.js
@@ -1,13 +1,16 @@
-import { get, set } from "./indexed-db.js";
+import { get, set } from './indexed-db.js';
 
 // constants
 const DEFAULT_DAILY = 8.4;
-const DAILY_KEY = "dailyHours";
-const ACCUMULATED_HOURS_KEY = "accumulatedHours";
+const DAILY_KEY = 'dailyHours';
+const ACCUMULATED_HOURS_KEY = 'accumulatedHours';
+const COMING = 0;
+const GOING = 1;
 
-// module globals
+// module globals / state
 
 let date = new Date();
+let numTimePairs = 0;
 
 // helper functions
 const formatDateAsKey = (date) =>
@@ -16,110 +19,112 @@ const formatDateAsKey = (date) =>
 const timeToDecimal = (hours, minutes) =>
   parseInt(hours, 10) + parseInt(minutes, 10) / 60; // hours in 24h format
 
-const getItems = async () => {
-    const key = formatDateAsKey(date);
-    const items = await get(key);
-    return {items, key};
+// time format: hh:mm, 24-hour time
+const human2decimalTime = (humanReadableTime) => {
+  let time = humanReadableTime;
+  if (humanReadableTime === undefined) {
+    const now = new Date();
+    const _minutes = now.getMinutes();
+    const _hours = now.getHours();
+    time = `${_hours}:0${_minutes}`;
+  }
+
+  const [ok, hours, minutes] = `${time}`.match(/^(\d{1,2}):(\d{2,3})$/);
+  if (!ok) {
+    return null;
+  }
+
+  return timeToDecimal(hours, minutes);
 };
 
-const next = (offset = 1) => {
-    date = date.setDate(date.getDate() + offset);
-    return date;
-}
-
-const previous = () => next(-1);
-
-
-const daily = async (hours) => {
-    // fetch persisted daily goal (in decimal hours, e.g. 8.4)
-    let storedHours = (await get(DAILY_KEY)) || DEFAULT_DAILY;
-    // caller just wants to *get* stored hours?
-    if (hours === undefined) {
-	// yes
-      return storedHours;
-    }
-    // set
-    await set(DAILY_KEY, hours);
-    return hours;
+// API functions
+const getTimePairs = async () => {
+  const key = formatDateAsKey(date);
+  const timePairs = await get(key);
+  numTimePairs = timePairs.length;
+  return { timePairs, key };
 };
 
-const yearly = async (deltaHours) => {
-    const storedAccumulatedHours = (await get(ACCUMULATED_HOURS_KEY)) || 0.0;
-    // get
-    if (deltaHours === undefined) {
-      return storedAccumulatedHours;
-    }
-    // update
-    const newAccumulatedHours = storedAccumulatedHours + deltaHours;
-    // set
-    await set(ACCUMULATED_HOURS_KEY, newAccumulatedHours);
-    return newAccumulatedHours;
+// navigate to next day
+const nextDay = (offset = 1) => {
+  date = date.setDate(date.getDate() + offset);
+  return date;
 };
 
-const add = async (time, which, newIndex, newRangeIndex) {
-    let decimalTimeOnly = false;
-    if (time === undefined) {
-      const now = new Date();
-      const _minutes = now.getMinutes();
-      const _hours = now.getHours();
-      time = `${_hours}:0${_minutes}`;
-      decimalTimeOnly = which === undefined;
-    }
+// navigate to previous day
+const previousDay = () => nextDay(-1);
 
-    const [ok, hours, minutes] = `${time}`.match(/^(\d{1,2}):(\d{2,3})$/);
-    if (!ok) {
-      return false;
-    }
-
-    const timeDecimal = timeToDecimal(hours, minutes);
-
-    if (decimalTimeOnly) {
-      return timeDecimal;
-    }
-
-    // time format: hh:mm
-    const {items, key} = await this.getItems();
-
-    const { index = 0, rangeIndex = 0, dateKey } = this;
-    const nextRange = rangeIndex > 1;
-    let isEdit = false;
-
-    if (newIndex === undefined) {
-      newIndex = index + (nextRange ? 1 : 0);
-    } else {
-      isEdit = true;
-    }
-
-    items[newIndex] = items[newIndex] || [];
-
-    if (newRangeIndex === undefined) {
-      newRangeIndex = which !== undefined ? which : nextRange ? -1 : rangeIndex;
-    }
-
-    items[newIndex][newRangeIndex] = timeDecimal;
-
-    await set(dateKey, items);
-
-    if (!isEdit) {
-      this.rangeIndex = newRangeIndex + 1;
-      this.index = newIndex;
-    }
-
-    return true;
+const dailyHours = async (hours) => {
+  // fetch persisted daily goal (in decimal hours, e.g. 8.4)
+  let storedHours = (await get(DAILY_KEY)) || DEFAULT_DAILY;
+  // caller just wants to *get* stored hours?
+  if (hours === undefined) {
+    // yes
+    return storedHours;
+  }
+  // set
+  await set(DAILY_KEY, hours);
+  return hours;
 };
 
-const edit = (index, time, which) => add(time, which, index, which);
+const yearlyHours = async (deltaHours) => {
+  const storedAccumulatedHours = (await get(ACCUMULATED_HOURS_KEY)) || 0.0;
+  // get
+  if (deltaHours === undefined) {
+    return storedAccumulatedHours;
+  }
+  // update
+  const newAccumulatedHours = storedAccumulatedHours + deltaHours;
+  // set
+  await set(ACCUMULATED_HOURS_KEY, newAccumulatedHours);
+  return newAccumulatedHours;
+};
 
-const delete = async (index) => {
-    // get items
-    const {items, key} = await getItems();
-    // remove the item under index
-    const deleted = items.splice(index, 1);
-    // persist updated items
-    await set(key, items);
-    // return the removed item to the caller
-    return deleted[0];
+const append = (offset = 0) => numTimePairs + offset;
+
+const last = () => append(-1);
+
+// add a time event (which = {COMING, GOING}) at a certain time pair (where = index into time pairs)
+// Examples:
+// - addTimeEvent(COMING, append): append new time pair, fill in coming part
+// - addTimeEvent(GOING, last): edit last-appended time pair, fill in going part
+// - addTimeEvent(COMING, 3, '08:44'): edit time pair via random access, overwrite coming part
+const addTimeEvent = async (which, where, humanReadableTime) => {
+  const timeDecimal = human2decimalTime(humanReadableTime);
+
+  const { timePairs, key } = await getTimePairs(); // side-effect: sets numTimePairs
+
+  const _where = typeof where === 'function' ? where() : where;
+
+  timePairs[_where] = timePairs[_where] || [];
+
+  timePairs[_where][which] = timeDecimal;
+
+  await set(key, timePairs);
+};
+
+const deleteTimePair = async (where) => {
+  // get timePairs
+  const { timePairs, key } = await getTimePairs();
+  // remove the item under where
+  const deleted = timePairs.splice(where, 1);
+  // persist updated timePairs
+  await set(key, timePairs);
+  // return the removed item to the caller
+  return deleted[0];
 };
 
 // API
-export {next, previous, daily, yearly, add, edit, delete}; 
+export {
+  COMING,
+  GOING,
+  getTimePairs,
+  nextDay,
+  previousDay,
+  dailyHours,
+  yearlyHours,
+  append,
+  last,
+  addTimeEvent,
+  deleteTimePair,
+};
